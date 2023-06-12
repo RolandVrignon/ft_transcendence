8525fe356197f9fe4aae9f8d71e7de99f0df6325
Hans Alvarez Herencia <hansalvarezherencia@protonmail.com>
2023-06-12 17:32:41 +0200

    +link into SolidFrame

--- srcs/requirements/front/src/components/SolidFrame/SolidFrame.tsx
+++ srcs/requirements/front/src/components/SolidFrame/SolidFrame.tsx
@@ -2,57 +2,65 @@ import React from "react";
 import "./SolidFrame.scss";
 
 type SolidFrameProps = {
-  className?: string;
-  txt_1?: string;
-  txt_2?: string;
+	className?: string;
+	txt_1?: string;
+	txt_2?: string;
 	fontSize?: string;
-  borderColor?: string;
-  backgroundColor?: string;
-  borderRadius?: string;
-  borderWidth?: string;
-  width?: string | number; // Allow numbers for percentages
-  height?: string | number; // Allow numbers for percentages
+	borderColor?: string;
+	backgroundColor?: string;
+	borderRadius?: string;
+	borderWidth?: string;
+	width?: string | number; // Allow numbers for percentages
+	height?: string | number; // Allow numbers for percentages
 	children?: React.ReactNode;
+	link?: string;
 };
 
 function SolidFrame({
-    className,
-    txt_1,
-    txt_2,
+		className,
+		txt_1,
+		txt_2,
 		fontSize= '32px',
-    borderColor = 'black',
-    backgroundColor = 'transparent',
-    borderRadius = '10px',
-    borderWidth = '1px',
-    width,
-    height,
-		children
-  }: SolidFrameProps) {
+		borderColor = 'black',
+		backgroundColor = 'transparent',
+		borderRadius = '10px',
+		borderWidth = '1px',
+		width,
+		height,
+		children,
+		link
+	}: SolidFrameProps) {
 
-  return (
-    <div
-      className={`solid-frame ${className}`}
-      style={{
-        borderColor, 
-        backgroundColor, 
-        borderRadius, 
-        borderWidth,
-        borderStyle: 'solid',
-        width: typeof width === 'number' ? `${width}%` : width, 
-        height: typeof height === 'number' ? `${height}%` : height, 
-      }}
-    >
-		{children}	
+	return (
+		<div
+			className={`solid-frame ${className}`}
+			style={{
+				borderColor,
+				backgroundColor,
+				borderRadius,
+				borderWidth,
+				borderStyle: 'solid',
+				width: (typeof width === 'number') ? `${width}%` : width,
+				height: (typeof height === 'number') ? `${height}%` : height,
+			}}
+		>
+		{children}
 			<span className="text-content">
 				<span className='grey' style={{fontSize: fontSize}}>
-				  {txt_1}
+					{txt_1}
 				</span>
 				<span className='black' style={{fontSize: fontSize}}>
-				  {txt_2}
+				{link ? (
+						<a href={link}>{txt_2}</a>
+					) : (
+						<p>{txt_2}</p>
+					)
+				}
+					{txt_2}
 				</span>
 			</span>
-    </div>
-  );
+		</div>
+	);
 }
 
 export default SolidFrame;
