8a94dc4e4cfc2684d295f698c3e2488cf68948c8
Hans Alvarez Herencia <hansalvarezherencia@protonmail.com>
2023-06-12 12:07:54 +0200

    Cleaning components

--- srcs/requirements/front/src/components/Separator/Separator.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './Separator.scss' 
-
-function Separator() {
-	return <div className="separator"></div>;
-}
-
-export default Separator;
--- srcs/requirements/front/src/components/StandardBox/StandardBox.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './StandardBox.scss';
-
-function StandardBox() {
-	return <div className="standard-box"></div>;
-}
-
-export default StandardBox;
