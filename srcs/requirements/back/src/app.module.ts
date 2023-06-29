647c80ccdf7fe63500a6cd182174899c39940e68
oscobouhou <osc.boutarfa@gmail.com>
2023-06-29 14:10:09 +0200

    [cleaned up the controller and modules in back for good reading]

--- srcs/requirements/back/src/app.module.ts
+++ srcs/requirements/back/src/app.module.ts
@@ -1,17 +1,16 @@
 import { Module } from '@nestjs/common';
-import { ConnectController, SearchController, SocialInteractController } from './controllers/auth/auth.controller';
+import { ConnectController } from './controllers/auth.controller'
+import { SearchController } from './controllers/search.controller'
+import { SocialInteractController } from './controllers/social.controller'
 import { MessagesModule } from './messages/messages.module';
 import { PongGateway } from './pong-gateway/pong-gateway.gateway'
 import { JwtModule } from '@nestjs/jwt'
 import { jwtConstants } from './jwt/jwt.constant'
+import { AuthModule } from './modules/auth.module';
 
 @Module({
-  imports: [MessagesModule,
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '1h' }
-    })
-  ],
+  imports: [MessagesModule, JwtModule.register({ secret: jwtConstants.secret, signOptions: { expiresIn: '1h' }}),
+  AuthModule],
   controllers: [SearchController, ConnectController, SocialInteractController],
   providers: [PongGateway]
 })
